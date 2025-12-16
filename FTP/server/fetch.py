1aeca620a97b728c1f5b4f1bef233216af59c2ad
operations <operations@treecap.nl>
2025-12-16 15:28:49 +0100

    requirements

--- FTP/server/fetch.py
+++ FTP/server/fetch.py
@@ -7,9 +7,11 @@ import zipfile
 from datetime import datetime, timedelta
 import subprocess
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.insert(0, BASE_DIR)
+
 from Functions.system.path import root_dir, base_dir
 
-sys.path.append(base_dir)
 
 
 
